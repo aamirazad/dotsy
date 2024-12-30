b32e95f5fa9e44215090d0c31c796638d76fbced
Aamir Azad <aamirmazad@gmail.com>
2024-12-30 11:40:42 -0500

    switch to pyqt
    aparently better docs

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 # For command line arguments
 import sys
 
+# Create a qt widget (window)
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Dotsy")
+
+        button = QPushButton("Press me!")
+
+        self.setFixedSize(QSize(400,300))
+
+        self.setCentralWidget(button)
+
 app = QApplication(sys.argv)
 
-# Create a qt widget (window)
-window = QWidget()
+window = MainWindow()
 window.show() # Windows are hidden by default
 
 # Start the exec loop
